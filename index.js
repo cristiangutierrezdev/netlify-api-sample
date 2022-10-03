768f99f1ecd5424e17da5bb9f1548c1c7152dcbf
Cristian Gutierrez <cristiandisenadorvisual@gmail.com>
2022-10-03 18:27:23 -0500

    Fix source dir on deploy file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = process.PORT || 3000
-const routes = require('../routes')
+const routes = require('./routes')
 const cors = require('cors');
 const serverless = require('serverless-http');
 
